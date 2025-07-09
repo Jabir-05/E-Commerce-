a4dfd5a0997f000b487b7be964be2bb507a3f9a7
Jabir-05 <knock5jabir@gmail.com>
2025-07-10 02:39:47 +0530

    workinprocess

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
+
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
 import Category  from "./Category/Category";
 import Products  from "../Products/Products";
+import {fetchDataFromApi} from "../../utils/api";
 const Home = () => {
+
+    useEffect (() => {
+     fetchDataFromApi()
+    },[])
+
+    const getCategories = () =>{
+        fetchDataFromApi("/api/categories?populates=*").then((res) => console.log(res));
+
+    }
     return <div>
         <Banner/>
         <div className="main-content">
