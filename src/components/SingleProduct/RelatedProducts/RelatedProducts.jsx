30dabac92e7fc09d5001bba17734f3f6cf053135
Jabir-05 <knock5jabir@gmail.com>
2025-07-10 01:15:12 +0530

    Edited

--- src/components/SingleProduct/RelatedProducts/RelatedProducts.jsx
+++
@@ -1,9 +0,0 @@
-import Products from "../../Products/Products";
-
-function RelatedProducts() {
-    return <div className="related-products">
-        <Products headingText = "Related Produts"/>
-    </div>;
-}
-
-export default RelatedProducts;
