08050563e06c54402ade6ba402e2a81f91837ee0
Jabir-05 <knock5jabir@gmail.com>
2025-07-15 22:52:08 +0530

    Test

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -3,30 +3,34 @@ import { useEffect } from "react";
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
-import Category  from "./Category/Category";
-import Products  from "../Products/Products";
-import {fetchDataFromApi} from "../../utils/api";
+import Category from "./Category/Category";
+import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
+
 const Home = () => {
+    useEffect(() => {
+        getCategories();
+    }, []);
 
-    useEffect (() => {
-     fetchDataFromApi()
-    },[])
+    const getCategories = () => {
+        fetchDataFromApi("/api/categories?populate=*")
+            .then((res) => console.log(res));
+    };
 
-    const getCategories = () =>{
-        fetchDataFromApi("/api/categories?populates=*").then((res) => console.log(res));
-
-    }
-    return <div>
-        <Banner/>
-        <div className="main-content">
-            <div className="layout">
-               <Category/>
-               <Products headingText = "Popular Produts"/>
-               
+    return (
+        <div>
+            <Banner />
+            <div className="main-content">
+                <div className="layout">
+                    <Category />
+                    <Products headingText="Popular Products" />
+                </div>
             </div>
         </div>
-        
-    </div>;
+    );
 };
 
 export default Home;
+
+
+// jabir imteyaz 
