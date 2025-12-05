528c3682ef73f4725ae7dd7b4f487b0a0ffd6fed
Jabir-05 <knock5jabir@gmail.com>
2025-12-05 21:39:16 +0530

    Error

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect,useContext } from "react";
 
 import "./Home.scss";
 
@@ -6,15 +6,32 @@ import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
 import { fetchDataFromApi } from "../../utils/api";
+import { Context } from "../../utils/context";
+
 
 const Home = () => {
+    const{categories,setCategories,products,setProducts} = useContext(Context);
+
     useEffect(() => {
+        getProducts();
         getCategories();
     }, []);
 
+    const getProducts = () => {
+        fetchDataFromApi("/api/products?populate=*")
+            .then((res) => {
+                console.log(res)
+        setProducts(res);
+            }
+    );
+    };
     const getCategories = () => {
         fetchDataFromApi("/api/categories?populate=*")
-            .then((res) => console.log(res));
+            .then((res) => {
+                console.log(res)
+        setCategories(res);
+            }
+    );
     };
 
     return (
@@ -22,8 +39,8 @@ const Home = () => {
             <Banner />
             <div className="main-content">
                 <div className="layout">
-                    <Category />
-                    <Products headingText="Popular Products" />
+                    <Category categories={categories} />
+                    <Products products={products} headingText="Popular Products" />
                     
                 </div>
             </div>
--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -7,7 +7,7 @@ const params = {
 
 export const fetchDataFromApi = async (url) => {
   try {
-    const {data} = await axios.get(process.env.REACT_APP_DEV_URL + url, params)
+    const {data} = await axios.get(process.env.REACT_APP_DEV_URL + url, params);
     return data;
   } catch (error) {
     console.log(error);
